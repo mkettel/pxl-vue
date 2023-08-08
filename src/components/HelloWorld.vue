c81708bd4bf0d2574abdad2f83f2ad20a6ea5110
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-08-08 15:39:22 -0400

    added the navbar up top instead of that bulky side view. will get navbar active with proper pages and routing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,48 @@
 <script setup>
+
+// define props for the component
 defineProps({
   msg: {
     type: String,
     required: true
+  },
+  subtitle: {
+    type: String,
+    required: true
   }
 })
+
 </script>
 
 <template>
   <div class="greetings">
+    <!-- Add the prop message that was passed from App.vue -->
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h3>{{ subtitle }}</h3>
   </div>
 </template>
 
 <style scoped>
+
+.greetings {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+/* style for the title */
 h1 {
   font-weight: 500;
-  font-size: 2.6rem;
+  font-size: 2rem;
   position: relative;
   top: -10px;
+  margin: 0px;
 }
-
+/* style for subtitle */
 h3 {
-  font-size: 1.2rem;
+  font-size: .9rem;
+  margin: 0px;
+  position: relative;
+  bottom: 10px;
 }
 
 .greetings h1,
@@ -35,7 +50,7 @@ h3 {
   text-align: center;
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 860px) {
   .greetings h1,
   .greetings h3 {
     text-align: left;
