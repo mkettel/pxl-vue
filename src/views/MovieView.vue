d03b80ac869d9b813dd9eac55304818c8526d388
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-08-08 16:24:54 -0400

    added date format method. gets year, month, day and interpolates values to readable format

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -12,6 +12,20 @@ onMounted(() => {
   movieStore.fetchMovies();
 });
 
+// Convert the date to a readable format
+const formatReleaseDate = (timestamp) => {
+    // (converting seconds to milliseconds) saving the date in a variable
+    const date = new Date(timestamp * 1000);
+
+    // Use the Date object's methods to get the components of the date
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed, so we add 1
+    const day = String(date.getDate()).padStart(2, '0');
+
+    // Format the date in "YYYY-MM-DD" format
+    return `${year}-${month}-${day}`;
+  };
+
 </script>
 
 <template>
@@ -31,7 +45,7 @@ onMounted(() => {
         <tr v-for="movie in movieStore.movies" :key="movie.id">
           <td>{{ movie.name }}</td>
           <td>{{ movie.synopsis }}</td>
-          <td>{{ movie.createdAt }}</td>
+          <td>{{ formatReleaseDate(movie.createdAt) }}</td>
         </tr>
       </tbody>
     </table>
